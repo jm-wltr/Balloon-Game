90feeaafab40f854aebc24335f6a64d9306ce080
jm-wltr <90411864+jm-wltr@users.noreply.github.com>
2021-11-26 13:20:32 +0100

    Added life system

--- sword_game.ino
+++ sword_game.ino
@@ -1,21 +1,27 @@
 int analogPin = A0;
 int val = 0;  // variable to store the analog value read
 int minsense = 300; //minimum analog value to trigger sensore in condition below
+int lives = 6;
 
 
 void setup() {
   Serial.begin(9600);
-  pinMode(13, OUTPUT); // Pin 13 is the default built-in LED on the arduino
-}
-
-void loop() {
-  val = analogRead(analogPin);
-  Serial.println(val); //useful when calibrating sensor for minsense value
-  delay(10);
-  if (val < minsense){
-    digitalWrite(13, HIGH);
-    delay(3000);
-    digitalWrite(13, LOW);
+  for (int i = 1; i < 7; i++) {
+    pinMode(i, OUTPUT);
+    digitalWrite(i, HIGH);
   }
+ delay(1000);
+}
   
+void loop() {
+  if (lives > 1) {
+    val = analogRead(analogPin);
+    Serial.println(val); //useful when calibrating sensor for minsense value
+    delay(10);
+    if (val < minsense){
+      digitalWrite(lives, LOW);
+      delay(1000);
+      lives = lives-1;
+    }
+  }
 }
