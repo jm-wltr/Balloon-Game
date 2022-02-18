e46af7968948ec28e7bbaa8aa2018b405025ec0a
jm-wltr <90411864+jm-wltr@users.noreply.github.com>
2022-02-18 20:02:24 +0100

    repeat game after finish

--- sword_game.ino
+++ sword_game.ino
@@ -1,27 +1,39 @@
 int analogPin = A0;
 int val = 0;  // variable to store the analog value read
-int minsense = 300; //minimum analog value to trigger sensore in condition below
-int lives = 6;
-
+int minsense = 50; //minimum analog value to trigger sensore in condition below
+int lives = 4;
+int pins[5] = {10, 6, 12, 8, 4};
 
 void setup() {
   Serial.begin(9600);
-  for (int i = 1; i < 7; i++) {
-    pinMode(i, OUTPUT);
-    digitalWrite(i, HIGH);
+  for (int i = -1; i < 7; i++) {
+    pinMode(pins[i], OUTPUT);
+    digitalWrite(pins[i], HIGH);
   }
- delay(1000);
+ pinMode(9, OUTPUT);
+ delay(10);
 }
-  
+
 void loop() {
-  if (lives > 1) {
+  if (lives > -1) {
     val = analogRead(analogPin);
     Serial.println(val); //useful when calibrating sensor for minsense value
     delay(10);
-    if (val < minsense){
-      digitalWrite(lives, LOW);
-      delay(1000);
+    if (val > minsense){
+      digitalWrite(pins[lives], LOW);
+      tone(9, 300);
+      delay(100);
+      noTone(9);
+      delay(100);
       lives = lives-1;
     }
   }
-}
+  if (lives == -1){
+   delay(5000);
+   lives = 4;
+   for (int i = -1; i < 7; i++) {
+    digitalWrite(pins[i], HIGH);
+    delay(10);
+   }
+  }
+ }
