ec2e13ede7c0a583bb29fa1468c1a539a1166678
jm-wltr <90411864+jm-wltr@users.noreply.github.com>
2022-02-20 19:00:04 +0100

    cleaned up, added jingle, other modifications

--- sword_game.ino
+++ sword_game.ino
@@ -1,39 +1,59 @@
-int analogPin = A0;
+int analogPin = A0; //analog input pin for piezoelectric sensor
+int buzzerPin = 3;
+int pins[5] = {12, 10, 9, 6, 4}; //LED pins
 int val = 0;  // variable to store the analog value read
-int minsense = 50; //minimum analog value to trigger sensore in condition below
-int lives = 4;
-int pins[5] = {7, 10, 9, 4, 5};
+int minsense = 50; //minimum analog value to trigger sensor
+int lives = 5; //initial lives
+int chord[5] = {196, 294, 392, 494, 784}//frequencies for sound at the end
 
-void setup() {
+//turn on all LEDs
+void setup(){
   Serial.begin(9600);
-  for (int i = -1; i < 7; i++) {
+  for(int i = 0; i < 5; i++){
     pinMode(pins[i], OUTPUT);
     digitalWrite(pins[i], HIGH);
   }
- pinMode(3, OUTPUT);
- delay(10);
+  pinMode(buzzerPin, OUTPUT);
+  delay(10);
 }
 
-void loop() {
-  if (lives > -1) {
+//main loop
+void loop(){
+  //detect hits and turn off lights
+  if(lives > 0){
     val = analogRead(analogPin);
-    Serial.println(val); //useful when calibrating sensor for minsense value
+    Serial.println(val); //useful when calibrating for minsense value
     delay(10);
     if (val > minsense){
-      digitalWrite(pins[lives], LOW);
-      tone(3, 300);
+      digitalWrite(pins[lives-1], LOW);
+      tone(buzzerPin, 300);
       delay(100);
-      noTone(3);
+      noTone(buzzerPin);
       delay(100);
       lives = lives-1;
     }
   }
-  if (lives == -1){
-   delay(5000);
-   lives = 4;
-   for (int i = -1; i < 7; i++) {
-    digitalWrite(pins[i], HIGH);
-    delay(10);
-   }
+  //game reset and chord jingle
+  if (lives == 0){
+    lives = 4;
+    for(int i = 0; i < 5; i++){
+      pinMode(pins[i], OUTPUT);
+      digitalWrite(pins[i], HIGH);
+      tone(buzzerPin, chord[i]);
+      delay(100);
+      digitalWrite(pins[i-1], LOW);
+      noTone(buzzerPin);
+    }
+    delay(200);
+    tone(buzzerPin, 988);
+    for(int i = 0; i < 5; i++){
+      digitalWrite(pins[i], HIGH);
+    }
+    delay(1500);
+    for(int i = 0; i < 5; i++){
+      digitalWrite(pins[i], LOW);
+    }
+    noTone(buzzerPin);
+    delay(2800);
   }
- }
+}
